8b873503c0f85f4a9d534f2fb4a3498cdb37516d
Nikita Bystrov <arttsesoft@gmail.com>
2016-08-18 14:42:21 +0300

    Added script

---
+++ index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/**
+ * Check if URL is relative without domain
+ *
+ * @param {String} str - URL to check
+ * @returns {Boolean}
+ */
+module.exports = function ( str ) {
+
+  if ( typeof str !== 'string' ) {
+
+    throw new TypeError ( 'Expected a string, not ' + typeof str );
+
+  }
+
+  /** Trim */
+  str = str.trim ();
+
+  /** Check // */
+  if ( str.indexOf ( '//' ) === 0 ) {
+
+    return false;
+
+  }
+
+  /** Check domain */
+  return !/^(?:\/|)(?:\w+(?:\.|:))/.test ( str );
+
+};
